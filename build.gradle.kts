bc88d1aab576a2e702e494a717f60f6c09fbe698
Thiago Oliveira <pintowar@gmail.com>
2025-11-04 23:53:42 -0300

    adding external dependencies file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.apache.tools.ant.filters.ReplaceTokens
 
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("kapt") version "2.2.21"
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    id("org.graalvm.buildtools.native") version "0.11.2"
-    id("org.jetbrains.kotlinx.kover") version "0.9.3"
-    id("org.sonarqube") version "7.0.1.6134"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.graalvm.native)
+    alias(libs.plugins.kotlinx.kover)
+    alias(libs.plugins.sonarqube)
     id("idea")
     application
 }
@@ -35,23 +35,20 @@ repositories {
 }
 
 dependencies {
-    // Picocli for command-line parsing
-    implementation("info.picocli:picocli:4.7.5")
-    kapt("info.picocli:picocli-codegen:4.7.5") // Annotation processor for GraalVM
+    implementation(libs.picocli)
+    kapt(libs.picocli.codegen)
 
-    implementation("io.konform:konform-jvm:0.11.0")
-    implementation("org.choco-solver:choco-solver:4.10.18")
-    implementation("com.fasterxml.uuid:java-uuid-generator:5.1.1")
-    implementation("org.jgrapht:jgrapht-core:1.5.2")
-    implementation("org.apache.commons:commons-math3:3.6.1")
+    implementation(libs.konform.jvm)
+    implementation(libs.choco.solver)
+    implementation(libs.uuid.generator)
+    implementation(libs.jgrapht.core)
+    implementation(libs.commons.math3)
 
-    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.11.2")
-    implementation("org.jetbrains.lets-plot:lets-plot-image-export:4.7.3")
+    implementation(libs.lets.plot.kotlin.jvm)
+    implementation(libs.lets.plot.image.export)
 
-    testImplementation("io.mockk:mockk-jvm:1.14.6")
-    testImplementation("io.kotest:kotest-runner-junit5:6.0.4")
-    testImplementation("io.kotest:kotest-assertions-core:6.0.4")
-    testImplementation("io.kotest:kotest-assertions-konform-jvm:6.0.4")
+    testImplementation(libs.mockk.jvm)
+    testImplementation(libs.bundles.kotest)
 }
 
 application {
