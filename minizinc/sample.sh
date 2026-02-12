bf0053ae36da364cd7828d209d90424f31771452
Thiago Oliveira <pintowar@gmail.com>
2026-02-11 21:54:19 -0300

    adding symmetry break constraint

--- minizinc/sample.sh
+++ minizinc/sample.sh
@@ -1,3 +1,1 @@
-!#/bin/bash
-
-minizinc --output-mode json --solver org.choco.choco -p 15 -t 3000 scheduler.mzn sample04.dzn | python3 visualize_schedule.py
+minizinc --output-mode json --solver org.choco.choco -p 15 -t 3000 scheduler.mzn sample04.dzn | uv run visualize_schedule.py
