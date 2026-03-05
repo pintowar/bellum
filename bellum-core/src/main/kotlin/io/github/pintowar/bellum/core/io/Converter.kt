3a918cd61f639ffb4961da1014c403cbcf623bfa
Thiago Oliveira <pintowar@gmail.com>
2026-03-04 22:23:17 -0300

    refactor(parser): adding writer and reader sub-packages to io package (renamed from parser)

---
+++ bellum-core/src/main/kotlin/io/github/pintowar/bellum/core/io/Converter.kt
@@ -0,0 +1,14 @@
+package io.github.pintowar.bellum.core.io
+
+interface Converter<I, O> {
+    fun convert(input: I): Result<O>
+}
+
+abstract class BaseConverter<I, O> : Converter<I, O> {
+    abstract fun doConvert(input: I): O
+
+    override fun convert(input: I): Result<O> =
+        runCatching {
+            doConvert(input)
+        }
+}
